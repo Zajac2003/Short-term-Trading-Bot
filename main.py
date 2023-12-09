739af89679dd83b843689568ab78603923165ada
Wojciech Zajączkowski <zajaczkowski.wojtek003@gmail.com>
2023-12-09 03:31:30 +0100

    DO NOT MERGE TO BUFOR in case somebody wanted
    nothing important nor usefull for now

--- main.py
+++ main.py
@@ -1,30 +1,68 @@
 from os import environ
 import requests
 
-API_KEY = 'AIzaSyBOEvN4OzAePlFp1fSRKWJlioA9r2WPZHw'
-AUTH_URL = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword'
-TRANSACTION_URL = 'http://platform.the-brawl.eu/api/transaction'
+#amount from source wallet
+def buyBitcoin(amount):
 
-data = {
-    "email": environ['THE_BRAWL_EMAIL'],
-    "password": environ['THE_BRAWL_PASSWORD'],
-    "returnSecureToken": True,
-}
-res = requests.post(AUTH_URL, json=data, params={'key': API_KEY}).json()
-id_token = res['idToken']
-#bruh
-res = requests.post(TRANSACTION_URL, headers={
-    'Authorization': f'Bearer {id_token}'
-}, json={
-    #USD WALLET      a8128857-ead7-491d-9b33-67c1226f4313
-    #BITCOIN WALLET  b15556c1-d545-4813-b4c0-ba33c72478a3
-    #ETHERIUM WALLET ace6007f-077d-4118-94ff-4cc12f36498a
-    "sourceWalletId": 'b15556c1-d545-4813-b4c0-ba33c72478a3',
-    "destWalletId": 'a8128857-ead7-491d-9b33-67c1226f4313',
-    "amountFromSourceWallet": 0.00002263,
-    "exchangeRate": 2.2,
-}, verify=False)
+    API_KEY = 'AIzaSyBOEvN4OzAePlFp1fSRKWJlioA9r2WPZHw'
+    AUTH_URL = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword'
+    TRANSACTION_URL = 'http://platform.the-brawl.eu/api/transaction'
 
-print(res.json())
-print("bum cyk cyk")
-print(669969)
+    data = {
+        "email": environ['THE_BRAWL_EMAIL'],
+        "password": environ['THE_BRAWL_PASSWORD'],
+        "returnSecureToken": True,
+    }
+    res = requests.post(AUTH_URL, json=data, params={'key': API_KEY}).json()
+    id_token = res['idToken']
+    #bruh
+    res = requests.post(TRANSACTION_URL, headers={
+        'Authorization': f'Bearer {id_token}'
+    }, json={
+        #USD WALLET      a8128857-ead7-491d-9b33-67c1226f4313
+        #BITCOIN WALLET  b15556c1-d545-4813-b4c0-ba33c72478a3
+        #ETHERIUM WALLET ace6007f-077d-4118-94ff-4cc12f36498a
+        "sourceWalletId": 'a8128857-ead7-491d-9b33-67c1226f4313',
+        "destWalletId": 'b15556c1-d545-4813-b4c0-ba33c72478a3',
+        "amountFromSourceWallet": amount,
+        "exchangeRate": 2.2,
+    }, verify=False)
+
+    print(res.json())
+
+def sellBitcoin(amount):
+
+    API_KEY = 'AIzaSyBOEvN4OzAePlFp1fSRKWJlioA9r2WPZHw'
+    AUTH_URL = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword'
+    TRANSACTION_URL = 'http://platform.the-brawl.eu/api/transaction'
+
+    data = {
+        "email": environ['THE_BRAWL_EMAIL'],
+        "password": environ['THE_BRAWL_PASSWORD'],
+        "returnSecureToken": True,
+    }
+    res = requests.post(AUTH_URL, json=data, params={'key': API_KEY}).json()
+    id_token = res['idToken']
+    #bruh
+    res = requests.post(TRANSACTION_URL, headers={
+        'Authorization': f'Bearer {id_token}'
+    }, json={
+        #USD WALLET      a8128857-ead7-491d-9b33-67c1226f4313
+        #BITCOIN WALLET  b15556c1-d545-4813-b4c0-ba33c72478a3
+        #ETHERIUM WALLET ace6007f-077d-4118-94ff-4cc12f36498a
+        "sourceWalletId": 'b15556c1-d545-4813-b4c0-ba33c72478a3',
+        "destWalletId": 'a8128857-ead7-491d-9b33-67c1226f4313',
+        "amountFromSourceWallet": amount,
+        "exchangeRate": 2.2,
+    }, verify=False)
+
+    print(res.json())
+
+operacja = int(input())
+if operacja == 1:
+    print("buying")
+    buyBitcoin(input())
+
+elif operacja == 2:
+    print("selling")
+    sellBitcoin(input())
--- scraping.py
+++ scraping.py
@@ -1,9 +1,15 @@
-from os import environ
 import requests
+from os import environ
+
+# Przykładowa strona do pobrania HTML
+url_to_scrape = "https://platform.the-brawl.eu/dashboard"
+
+# Jeśli potrzebujesz uwierzytelnienia, wykonaj odpowiednie kroki
+# W tym przykładzie używam tylko nagłówka z uwierzytelnieniem z poprzedniego kodu
 
 API_KEY = 'AIzaSyBOEvN4OzAePlFp1fSRKWJlioA9r2WPZHw'
 AUTH_URL = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword'
-TRANSACTION_URL = 'http://platform.the-brawl.eu/api/transaction'
+TRANSACTION_URL = 'https://platform.the-brawl.eu/dashboard'
 
 # Dane do uwierzytelnienia
 auth_data = {
@@ -13,21 +19,20 @@ auth_data = {
 }
 
 # Uwierzytelnianie
-auth_response = requests.post(AUTH_URL, json=auth_data, params={'key': API_KEY}).json()
+auth_response = requests.post(AUTH_URL, json=auth_data, params={'key': API_KEY}, verify=False).json()
 id_token = auth_response.get('idToken')
 
 if id_token:
     # Dalsze operacje, jeśli autentykacja się powiedzie
 
-    # Przykładowe pobranie danych ze strony (zmień URL według swoich potrzeb)
-    sample_url = 'https://example.com'
-    response = requests.get(sample_url)
+    # Wykonaj żądanie GET do strony do scrapowania
+    response = requests.get(url_to_scrape, headers={'Authorization': f'Bearer {id_token}'}, verify=False)
 
     if response.status_code == 200:
         # Jeśli pobranie się powiedzie
-        page_content = response.text
-        print("Pobrano zawartość strony:")
-        print(page_content)
+        html_content = response.text
+        print("Pobrano kod HTML strony:")
+        print(html_content)
     else:
         print(f"Błąd podczas pobierania strony. Kod odpowiedzi: {response.status_code}")
 else:
