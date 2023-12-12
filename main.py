c7bcc3249ebb69f370c1dadf286fcfc9cfec07d0
Wojciech Zajączkowski <zajaczkowski.wojtek003@gmail.com>
2023-12-12 21:08:21 +0100

    IMPORTANT UPDATE
    Achieved to scrape data from CoinBase; but doubt  if it is valid, ale jebac. Wszystko elegancko w funkcji zrobione pół polish pół english elegancki kodzik dojebać jeszcze algorytm i bitcoin jest nasz pozderki

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from os import environ
 import requests
+import time
 
 #amount from source wallet
-def buyBitcoin(amount):
+def buyBitcoin(amountInDollars):
 
     API_KEY = 'AIzaSyBOEvN4OzAePlFp1fSRKWJlioA9r2WPZHw'
     AUTH_URL = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword'
@@ -24,13 +25,13 @@ def buyBitcoin(amount):
         #ETHERIUM WALLET ace6007f-077d-4118-94ff-4cc12f36498a
         "sourceWalletId": 'a8128857-ead7-491d-9b33-67c1226f4313',
         "destWalletId": 'b15556c1-d545-4813-b4c0-ba33c72478a3',
-        "amountFromSourceWallet": amount,
+        "amountFromSourceWallet": amountInDollars,
         "exchangeRate": 2.2,
     }, verify=False)
 
     print(res.json())
 
-def sellBitcoin(amount):
+def sellBitcoin(amountInBitcoins):
 
     API_KEY = 'AIzaSyBOEvN4OzAePlFp1fSRKWJlioA9r2WPZHw'
     AUTH_URL = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword'
@@ -52,7 +53,7 @@ def sellBitcoin(amount):
         #ETHERIUM WALLET ace6007f-077d-4118-94ff-4cc12f36498a
         "sourceWalletId": 'b15556c1-d545-4813-b4c0-ba33c72478a3',
         "destWalletId": 'a8128857-ead7-491d-9b33-67c1226f4313',
-        "amountFromSourceWallet": amount,
+        "amountFromSourceWallet": amountInBitcoins,
         "exchangeRate": 2.2,
     }, verify=False)
 
@@ -70,9 +71,23 @@ def menu():
         sellBitcoin(input())
         print("Sold succesfully")
 
+def currentBitcoinExchangeRate():
+    url = "https://api.coinbase.com/v2/exchange-rates?currency=BTC"
 
+    # Wykonaj GET request
+    response = requests.get(url)
 
+    # Sprawdź, czy odpowiedź jest udana (status code 200)
+    if response.status_code == 200:
+        # Pobierz dane z odpowiedzi
+        data = response.json()
+        kurs = data['data']['rates']['BUSD']
+        kurs = float(kurs)
+        return kurs
+    else:
+        # W przypadku nieudanej odpowiedzi, wyświetl komunikat o błędzie
+        print(f"Nieudany GET request. Status code: {response.status_code}")
 
-records = [20];
-
-print(type(records))
+while(True):
+    print(currentBitcoinExchangeRate())
+    time.sleep(2)
